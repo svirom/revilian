fc3bbc988e9fc647d900edc6b8f2223622bbfaa5
svirom <svirom@yahoo.com>
2019-12-05 15:40:11 +0200

    some semantic fixes

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,17 @@
-$(document).ready(function() {
+$(function() {
+
+  let width = $(document).width();
 
   // add arrows in dropdown menu on mobile
-  // if ($(document).width() < 1199) {
-    $('.main_menu').find('.dropdown > a').append('<img src="../img/arrow-down.png" alt="">');
-  // }
+  $('.main_menu').find('.dropdown > a').append('<img src="../img/arrow-down.png" alt="">');
 
   // scroll function
-  $(function () {
-    $(window).scroll(function () {
-      if ( ($(document).width() >= 1200) && ($(this).scrollTop() > 120) ) {
-        $('.main_nav').addClass("bkgr"); //background of main menu
-      } else {
-        $('.main_nav').removeClass("bkgr"); //background of main menu
-      }
-    });
+  $(window).scroll(function () {
+    if ( (width >= 1200) && ($(this).scrollTop() > 120) ) {
+      $('.main_nav').addClass("bkgr"); //background of main menu
+    } else {
+      $('.main_nav').removeClass("bkgr"); //background of main menu
+    }
   });
 
   // link on homepage header
@@ -34,7 +32,7 @@ $(document).ready(function() {
 
   // show/hide submenu by click on menu button (mobile)
 	$('.main_menu .dropdown').on('click', function(event) {
-		if ($(document).width() < 1200) {
+		if (width < 1200) {
       event.preventDefault();
       $(this).toggleClass('dropdown-active');
 			$(this).stop().find('.dropdown-menu').slideToggle(500);
@@ -45,10 +43,10 @@ $(document).ready(function() {
   $('.nav-opener').on('click', function(event) {
     event.preventDefault();
 
-    if ($(document).width() < 1200) {
+    if (width < 1200) {
       $(this).toggleClass('nav-active');
       $(this).next('.main_menu').toggleClass('main_menu-active');
     }
   });
 
-})
+});
